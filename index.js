312560d45347f2781f2f6fb4289bf60bbde61547
Tyson Romero <romennoodles23@yahoo.com>
2021-09-10 16:30:59 -0600

    can add new post to top of list

--- index.js
+++ index.js
@@ -3,34 +3,46 @@ const postBody = document.querySelector("#post-body")
 const blogList = document.querySelector("#blog-list")
 const postBtn = document.querySelector('#post-btn')
 const newPost = document.querySelector('#new-post')
+let postArr = []
 
-newPost.addEventListener('submit', (e) => {
-    e.preventDefault()
-
-    let title = postTitle.value
-    let body = postBody.value
-
-    const data = {
-        title: title,
-        body: body
-    } 
-    console.log(data);
-})
+const renderPost = (e) => {
+    let html = ""
+    for(let post of postArr) {
+        html += `
+        <h3>${post.title}</h3>
+        <p>${post.body}</p>
+        <hr />
+    `
+    }
+    blogList.innerHTML = html
+}
 
 fetch('https://apis.scrimba.com/jsonplaceholder/posts')
     .then(res => res.json())
     .then(data =>  {
-        const postArr = data.splice(0,5)
+        postArr = data.splice(0,5)
+        renderPost()
+    })
 
-        let html = ""
-
-        for(let post of postArr) {
-            html += `
-                <h3>${post.title}</h3>
-                <p>${post.body}</p>
-                <hr />
-            `
-        }
-        blogList.innerHTML = html
+    newPost.addEventListener('submit', (e) => {
+        e.preventDefault()
+        let titleVal = postTitle.value
+        let bodyVal = postBody.value
+        const data = {
+            title: titleVal,
+            body: bodyVal
+        } 
+        fetch('https://apis.scrimba.com/jsonplaceholder/posts', { 
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8'
+            }
+         })
+            .then(res => res.json())
+            .then(post => {
+                postArr.unshift(post)
+                renderPost()
+        })
     })
 
